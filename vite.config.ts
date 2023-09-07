9879ea330fc1f64aa7555aebbe81fb0a36ea6804
luohuidong <luohuidong01@126.com>
2023-09-07 23:57:28 +0800

    build: configure build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
+import { resolve } from "node:path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import packagejson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +13,18 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: Object.keys(packagejson.peerDependencies),
     },
   },
 });
